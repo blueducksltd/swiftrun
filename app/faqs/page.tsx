74f3e7e61a44eb5b77a12684836205bc8dc385ab
J-ARGS-INC <nsudekenechi2@gmail.com>
2025-11-10 14:28:03 +0100

    made new changes

--- app/faqs/page.tsx
+++ app/faqs/page.tsx
@@ -36,18 +36,26 @@ export default function FAQs() {
         "Every driver on our platform is verified and trained to handle deliveries with care. You can track your package in real time, contact your driver anytime, and receive instant updates until it's safely delivered.",
       isOpened: false,
     },
+    {
+      question: "How do I know my package is safe during delivery?",
+      answer:
+        "Every driver on our platform is verified and trained to handle deliveries with care. You can track your package in real time, contact your driver anytime, and receive instant updates until it's safely delivered.",
+      isOpened: false,
+    },
   ]);
   return (
     <div>
       <HeaderElem>
-        <div className="grid gap-3 md:w-[95%]">
-          <h1 className="font-heading text-5xl md:text-7xl font-extrabold text-blue pr-20">
+        <div className="flex flex-col gap-5 md:w-[95%]">
+          <h1 className="h-fit font-heading text-5xl md:text-7xl font-extrabold text-blue pr-20">
             Some of your FAQs
           </h1>
-          <p className="text-lg text-black">
-            We&rsquo;ve gathered answers to the questions users ask most, so you
-            can get help faster.
-          </p>
+          <div>
+            <p className="text-lg text-black  h-fit ">
+              We&rsquo;ve gathered answers to the questions users ask most, so
+              you can get help faster.
+            </p>
+          </div>
           <div className="grid grid-cols-1 md:grid-cols-2">
             <Link
               className="bg-cloudmist text-blue p-3 rounded-full text-center h-fit"
@@ -61,19 +69,33 @@ export default function FAQs() {
           {FAQs.map((faq, index) => (
             <div
               key={index}
-              className="bg-white    shadow-[0_10px_37px_rgba(0,0,0,0.1)] rounded-2xl text-black p-5"
+              className="bg-white    shadow-[0_10px_37px_rgba(0,0,0,0.1)] rounded-2xl text-black p-5 cursor-pointer"
+              onClick={() => {
+                setFAQs((prev) =>
+                  prev.map((item, faqIndex) => ({
+                    ...item,
+                    isOpened: faqIndex == index ? true : false,
+                  }))
+                );
+              }}
             >
               <div className="flex justify-between items-center">
-                <h1 className=" font-heading">{faq.question}</h1>
+                <h1 className=" font-heading w-[70%] md:w-full">{faq.question}</h1>
                 <div
-                  className={`w-7 h-7 rounded-full bg-whitetwo flex items-center justify-center ${
+                  className={`duration-300 w-7 h-7 rounded-full bg-whitetwo flex items-center justify-center ${
                     faq.isOpened ? "rotate-45" : "-rotate-45"
                   }`}
                 >
                   <FaArrowRight size={12} />
                 </div>
               </div>
-              <div className={`h-20 border-t mt-2 pt-2 border-whitetwo font-light text-sm `}>
+              <div
+                className={` duration-500 transition-[min-height] ${
+                  faq.isOpened
+                    ? "min-h-28 border-t mt-2 pt-2 border-whitetwo font-light text-[15px] font-primary"
+                    : "h-0 min-h-0  overflow-hidden"
+                } `}
+              >
                 <p>{faq.answer}</p>
               </div>
             </div>
