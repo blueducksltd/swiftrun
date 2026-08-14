f432d0f6f521497d6e988d1d8a59e1fc54d27e8f
godson-pius <godsonazubuike15@gmail.com>
2026-08-14 10:48:06 +0200

    Added download page for users and drivers

---
+++ app/(v2)/download-the-app/Driver.tsx
@@ -0,0 +1,58 @@
+'use client'
+import Image from "next/image";
+import Link from "next/link";
+import Appstore from "@/public/appstore.png";
+import { useOSRedirect } from "@/hooks/useOSRedirect";
+
+const STORE_LINKS = {
+  ios: "https://apps.apple.com/ng/app/swiftrun-driver/id6758965318",
+  android: "https://play.google.com/store/apps/details?id=com.swiftrun.driver",
+};
+
+export default function Driver() {
+  useOSRedirect(STORE_LINKS);
+
+  return (
+    <>
+      <div className="flex flex-col gap-5 order-1 md:order-0 max-w-[600px] mt-20">
+        <h1 className="h-fit font-heading text-5xl md:text-7xl font-extrabold text-blue ">
+          Join, Serve, Earn.
+        </h1>
+        <div>
+          <p className="text-lg text-black  h-fit ">
+            Join our network of trusted drivers and start earning on your own
+            terms.
+          </p>
+        </div>
+        <div className="grid grid-cols-2 gap-2">
+          <Link className="" href={STORE_LINKS.android}>
+            <Image
+              width={400}
+              height={400}
+              src={"/Playstore.png"}
+              alt="Google Store Icon"
+            />
+          </Link>
+
+          <Link className="" href={STORE_LINKS.ios}>
+            <Image
+              width={400}
+              height={400}
+              src={Appstore}
+              alt="Apple App Store Icon"
+            />
+          </Link>
+        </div>
+      </div>
+      <div className="md:col-span-2  flex justify-end relative mt-20">
+        <Image
+          alt="downloadAppUserBg"
+          src={"/join_serve.webp"}
+          width={550}
+          height={550}
+        />
+      </div>
+    </>
+  );
+}
+
---
+++ app/(v2)/download-the-app/[type]/page.tsx
@@ -0,0 +1,18 @@
+import User from "../User";
+import Driver from "../Driver";
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Download App",
+};
+export default async function downloadTheApp({
+  params,
+}: {
+  params: Promise<{ type: string }>;
+}) {
+  const { type } = await params;
+  return (
+    <div className="relative z-10 grid grid-cols-1 md:grid-cols-3 p-6 md:p-20  gap-14">
+      {type == "user" ? <User /> : <Driver />}
+    </div>
+  );
+}
